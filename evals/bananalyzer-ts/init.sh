5701f7694771377795f5846ca30a5e9c171efc9a
Navid Pour <navid.pour@outlook.com>
2024-09-29 21:09:44 -0700

    cleanup

--- evals/bananalyzer-ts/init.sh
+++ evals/bananalyzer-ts/init.sh
@@ -5,6 +5,8 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Clone the repository
 git clone https://github.com/reworkd/bananalyzer.git
+git -C bananalyzer checkout 49038a5619616893034385a9c07f3ac40d7a8b8d # In case the repo has a bad commit
+# Manually update the hash once in a while
 
 # Copy the static folder to the script's directory
 rm -rf "$SCRIPT_DIR/static"
