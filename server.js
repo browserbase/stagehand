dc5c019cf0eb7134cc7fcf13a1e0301596f9d262
Ayush Tiwari <tiwari.ayush2412@gmail.com>
2024-12-05 23:12:24 +0100

    [Brackett] Add new endpoints

--- server.js
+++ server.js
@@ -39,6 +39,32 @@ app.post('/run-task-steps', async (req, res) => {
   }
 });
 
+
+app.get('/run-notepad-task', async (req, res)=>{
+  try {
+    const stagehand = new Stagehand.Stagehand({
+      env: 'LOCAL',
+      verbose: 1,
+      debugDom: true,
+      domSettleTimeoutMs: 100,
+    });
+
+    await stagehand.init({ modelName: 'gpt-4o' });
+    await stagehand.page.goto("https://onlinenotepad.org/notepad");
+    await stagehand.act({
+      action: "find the content body of the page on notepad and enter %data%",
+      variables: {
+        data: "Hello, i am saving this data in the online notepad",
+      },
+    });
+
+  } catch (error) {
+    console.error('Error importing Stagehand or executing task:', error);
+    res.status(500).json({ error: 'Failed to execute task', details: error.message });
+  }
+}
+
+)
 // Start the server
 app.listen(PORT, () => {
   console.log(`Stagehand server listening on port ${PORT}`);
