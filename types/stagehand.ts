9933271ec607530739c0e00af0cd245c93244ba8
Anirudh Kamath <github@kamath.io>
2024-12-28 13:34:12 -0800

    StagehandPage and StagehandContext

--- types/stagehand.ts
+++ types/stagehand.ts
@@ -4,6 +4,8 @@ import { z } from "zod";
 import { LLMProvider } from "../lib/llm/LLMProvider";
 import { LogLine } from "./log";
 import { AvailableModel, ClientOptions } from "./model";
+import { StagehandPage } from "lib/StagehandPage";
+import { StagehandContext } from "lib/StagehandContext";
 
 export interface ConstructorParams {
   env: "LOCAL" | "BROWSERBASE";
@@ -87,3 +89,6 @@ export interface ObserveResult {
   selector: string;
   description: string;
 }
+
+export { StagehandPage };
+export { StagehandContext };
