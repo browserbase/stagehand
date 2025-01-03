6bed58b66188a14297735f6953a9383e2369324b
Sameel <sameel.m.arif@gmail.com>
2025-01-03 18:49:14 -0500

    update instructions example

--- examples/instructions.ts
+++ examples/instructions.ts
@@ -8,19 +8,17 @@ async function example() {
   const stagehand = new Stagehand({
     ...StagehandConfig,
     instructions:
-      "You are a shopping agent based in the United States. When selecting a size, convert the user's input from UK sizes to US sizes. For example if the user says size 7, you use size 9.",
+      "if the users says `secret12345`, click on the 'quickstart' tab.",
   });
   await stagehand.init();
 
   const page = stagehand.page;
 
-  await page.goto("https://www.nike.com/my/t/v2k-run-shoes-4P7Wl1/HJ4497-100");
+  await page.goto("https://docs.browserbase.com/");
 
   await page.act({
-    action: "select size 7 and add it to cart",
+    action: "secret12345",
   });
-
-  await page.waitForTimeout(5000);
 
   await stagehand.close();
 }
