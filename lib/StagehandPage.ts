9cf9e3dbb23b1332f0b1ac7ef8789c0377c9ec56
seanmcguire12 <seanmcguire1@outlook.com>
2025-01-23 11:48:43 -0800

    rm fullPage param

--- lib/StagehandPage.ts
+++ lib/StagehandPage.ts
@@ -505,7 +505,6 @@ export class StagehandPage {
         instruction,
         llmClient,
         useVision,
-        fullPage: false,
         requestId,
         domSettleTimeoutMs,
         useAccessibilityTree,
