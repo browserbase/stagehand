790dbce8d73e2a39ffefe726bdcb1f238737920e
Sean McGuire <seanmcguire1@outlook.com>
2025-07-17 10:34:57 -0700

    revert example

--- examples/example.ts
+++ examples/example.ts
@@ -1,34 +1,20 @@
-/**
- * This file is meant to be used as a scratchpad for developing new evals.
- * To create a Stagehand project with best practices and configuration, run:
- *
- * npx create-browser-app@latest my-browser-app
- */
-
 import { Stagehand } from "@browserbasehq/stagehand";
+import StagehandConfig from "../stagehand.config";
 
 async function example(stagehand: Stagehand) {
   /**
    * Add your code here!
    */
   const page = stagehand.page;
-
-  await page.goto(
-    "https://browserbase.github.io/stagehand-eval-sites/sites/nested-dropdown/",
-  );
-
-  await page.act(
-    "choose 'Smog Check Technician' from the 'License Type' dropdown",
-  );
+  await page.goto("https://docs.stagehand.dev");
+  await page.act("click the quickstart button");
 }
 
 (async () => {
   const stagehand = new Stagehand({
-    env: "LOCAL",
-    useAPI: false,
-    logInferenceToFile: true,
-    verbose: 2,
+    ...StagehandConfig,
   });
   await stagehand.init();
   await example(stagehand);
+  await stagehand.close();
 })();
