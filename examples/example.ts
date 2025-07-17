a3a4f86acc8ff2e6c4cfa72f43404864715fa629
Sean McGuire <seanmcguire1@outlook.com>
2025-07-17 10:36:43 -0700

    add comment back

--- examples/example.ts
+++ examples/example.ts
@@ -1,3 +1,9 @@
+/**
+ * This file is meant to be used as a scratchpad for developing new evals.
+ * To create a Stagehand project with best practices and configuration, run:
+ *
+ * npx create-browser-app@latest my-browser-app
+ */
 import { Stagehand } from "@browserbasehq/stagehand";
 import StagehandConfig from "../stagehand.config";
 
