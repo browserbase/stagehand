2a9d633abe14769f8d89c55c764745912241db33
Ashwin Kumar <ashwin@mercoa.com>
2025-08-01 07:10:07 -0700

    Left note about remaining skipped TS errors in inference.ts

--- lib/inference.ts
+++ lib/inference.ts
@@ -177,6 +177,9 @@ export async function extract({
     });
   const metadataEndTime = Date.now();
 
+  // NOTE: This is suspicious -- analyzed statically, this shouldn't work at all:
+  //       I suspect llmClient.createChatCompletion's return type in TS is wrong, so
+  //       I'm won't mess with this yet (same for all other LLMParsedResponse<T> usages)
   const {
     data: {
       completed: metadataResponseCompleted,
