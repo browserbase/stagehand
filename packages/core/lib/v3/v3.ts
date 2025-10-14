84d324b5dc46e3d9c061df7f7d9a6132c78d1310
miguel <miguelg71921@gmail.com>
2025-10-14 16:34:50 -0700

    fix comment

--- packages/core/lib/v3/v3.ts
+++ packages/core/lib/v3/v3.ts
@@ -646,7 +646,7 @@ export class V3 {
         }
         if (lbo.viewport?.width && lbo.viewport?.height) {
           chromeFlags.push(
-            `--window-size=${lbo.viewport.width + 50},${lbo.viewport.height + 85}`, // Added pixels to the window to fit the viewport, currently it doesn't display the full viewport
+            `--window-size=${lbo.viewport.width},${lbo.viewport.height + 87}`, // Added pixels to the window to account for the address bar
           );
         }
         if (typeof lbo.deviceScaleFactor === "number") {
