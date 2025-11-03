b34151530c42e2aaf02a45a289756ea8558bea9f
tkattkat <tkat@tkat.net>
2025-11-03 12:01:02 -0800

    revert testing changes

--- packages/core/examples/v3/targetedExtract.ts
+++ packages/core/examples/v3/targetedExtract.ts
@@ -4,21 +4,23 @@ import { z } from "zod";
 async function example(stagehand: Stagehand) {
   const page = stagehand.context.pages()[0];
   await page.goto(
-    "https://browserbase.github.io/stagehand-eval-sites/sites/jfk/",
+    "https://ambarc.github.io/web-element-test/stagehand-breaking-test.html",
   );
 
-  const extraction = await stagehand.extract(
-    "extract all the record file name and their corresponding links",
-    z.object({
-      records: z.array(
-        z.object({
-          file_name: z.string().describe("the file name of the record"),
-          link: z.url().describe("the link to the record"),
-        }),
-      ),
-    }),
+  await page
+    .deepLocator("/html/body/div[2]/div[3]/iframe/html/body/p")
+    .highlight({
+      durationMs: 5000,
+      contentColor: { r: 255, g: 0, b: 0 },
+    });
+
+  const reason = await stagehand.extract(
+    "extract the reason why script injection fails",
+    z.string(),
+    // selector: "// body > div.test-container > div:nth-child(3) > iframe >> body > p:nth-child(3)",
+    { selector: "/html/body/div[2]/div[3]/iframe/html/body/p[2]" },
   );
-  console.log(extraction);
+  console.log(reason);
 }
 
 (async () => {
