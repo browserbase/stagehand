92d45dbd815dd86d4759b3a02c579490d84374db
monadoid <sam.finton@gmail.com>
2025-11-17 14:17:10 +0100

    Synced test file with new changes to stagehand metrics

--- packages/core/tests/public-types.test.ts
+++ packages/core/tests/public-types.test.ts
@@ -104,18 +104,28 @@ describe("Stagehand public API types", () => {
     expectTypeOf<Stagehand.StagehandMetrics>().toEqualTypeOf<{
       actPromptTokens: number;
       actCompletionTokens: number;
+      actReasoningTokens: number;
+      actCachedInputTokens: number;
       actInferenceTimeMs: number;
       extractPromptTokens: number;
       extractCompletionTokens: number;
+      extractReasoningTokens: number;
+      extractCachedInputTokens: number;
       extractInferenceTimeMs: number;
       observePromptTokens: number;
       observeCompletionTokens: number;
+      observeReasoningTokens: number;
+      observeCachedInputTokens: number;
       observeInferenceTimeMs: number;
       agentPromptTokens: number;
       agentCompletionTokens: number;
+      agentReasoningTokens: number;
+      agentCachedInputTokens: number;
       agentInferenceTimeMs: number;
       totalPromptTokens: number;
       totalCompletionTokens: number;
+      totalReasoningTokens: number;
+      totalCachedInputTokens: number;
       totalInferenceTimeMs: number;
     }>();
   });
