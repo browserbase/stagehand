1a1aedf38a17002820fb94e5932c0040ee74ab8c
monadoid <sam.finton@gmail.com>
2025-11-20 18:06:51 +0100

    Flatten public type exports

--- packages/core/lib/v3/index.ts
+++ packages/core/lib/v3/index.ts
@@ -80,9 +80,89 @@ export {
   jsonSchemaToZod,
 } from "../utils";
 
-export { isZod4Schema, isZod3Schema, toJsonSchema } from "./zodCompat";
 
 export { connectToMCPServer } from "./mcp/connection";
 export { V3Evaluator } from "../v3Evaluator";
 
-export * from "./publicTypes";
+export type {
+  AnthropicJsonSchemaObject,
+  AISDKProvider,
+  AISDKCustomProvider,
+  LLMTool,
+  AvailableModel,
+  ModelProvider,
+  ClientOptions,
+  ModelConfiguration,
+} from "./types/public/model";
+
+export type {
+  ActOptions,
+  ActResult,
+  ExtractResult,
+  Action,
+  HistoryEntry,
+  ExtractOptions,
+  ObserveOptions,
+} from "./types/public/methods";
+
+export type {
+  AgentAction,
+  AgentResult,
+  AgentExecuteOptions,
+  AgentType,
+  AgentExecutionOptions,
+  AgentHandlerOptions,
+  ActionExecutionResult,
+  ToolUseItem,
+  AnthropicMessage,
+  AnthropicContentBlock,
+  AnthropicTextBlock,
+  AnthropicToolResult,
+  ResponseItem,
+  ComputerCallItem,
+  FunctionCallItem,
+  ResponseInputItem,
+  AgentInstance,
+  AgentProviderType,
+  AgentModelConfig,
+  AgentConfig,
+  AvailableCuaModel,
+} from "./types/public/agent";
+
+export type { LogLevel, LogLine, Logger } from "./types/public/logs";
+export type { StagehandMetrics } from "./types/public/metrics";
+export type {
+  V3Env,
+  LocalBrowserLaunchOptions,
+  V3Options,
+} from "./types/public/options";
+export type {
+  PlaywrightPage,
+  PatchrightPage,
+  PuppeteerPage,
+  Page,
+  AnyPage,
+  ConsoleListener,
+  LoadState,
+} from "./types/public/page";
+
+export type {
+  ChatMessage,
+  ChatMessageContent,
+  ChatMessageImageContent,
+  ChatMessageTextContent,
+  ChatCompletionOptions,
+  LLMResponse,
+  CreateChatCompletionOptions,
+  LLMUsage,
+  LLMParsedResponse,
+} from "./llm/LLMClient";
+
+export type {
+  StagehandZodSchema,
+  StagehandZodObject,
+  InferStagehandSchema,
+  JsonSchemaDocument,
+} from "./zodCompat";
+
+export type { JsonSchema, JsonSchemaProperty } from "../utils";
