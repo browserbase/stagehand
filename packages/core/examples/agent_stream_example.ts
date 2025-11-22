8d4f29dce72db00c22161e87ef17975290a5d900
tkattkat <tkat@tkat.net>
2025-11-21 17:29:04 -0800

    add streaming example

---
+++ packages/core/examples/agent_stream_example.ts
@@ -0,0 +1,53 @@
+
+import { Stagehand } from "../lib/v3";
+import dotenv from "dotenv";
+import chalk from "chalk";
+
+// Load environment variables
+dotenv.config();
+async function main() {
+  console.log(`\n${chalk.bold("Stagehand 🤘 Agent Streaming Example")}\n`);
+  // Initialize Stagehand
+  const stagehand = new Stagehand({
+    env: "LOCAL",
+    verbose: 2,
+    cacheDir: "stagehand-agent-cache",
+    logInferenceToFile: false,
+    experimental: true,
+  });
+
+  await stagehand.init();
+
+  try {
+    const page = stagehand.context.pages()[0];
+    await page.goto(
+      "https://amazon.com",
+    );
+    const agent = stagehand.agent({
+      model: "anthropic/claude-sonnet-4-5-20250929",
+      executionModel: "google/gemini-2.5-flash",
+      
+    });
+
+    const result = await agent.stream({
+      instruction:
+        "go to amazon, and seach for shampoo, stop after searching",
+      maxSteps: 20,
+    });
+     // stream the text 
+    for await (const delta of result.textStream) {
+      process.stdout.write(delta);
+    }
+    // stream everything ( toolcalls, messages, etc.)
+    // for await (const delta of result.fullStream) {
+    //   console.log(delta);
+    // }
+
+
+    const finalResult = await result.result;
+    console.log("Final Result:", finalResult);
+  } catch (error) {
+    console.log(`${chalk.red("✗")} Error: ${error}`);
+  }
+}
+main();
