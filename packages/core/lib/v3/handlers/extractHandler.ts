58d1e55d97cdc436f47f296959dcee8c53b7fbce
Sean McGuire <seanmcguire1@outlook.com>
2025-11-30 21:09:17 -0800

    revert temp handler changes

--- packages/core/lib/v3/handlers/extractHandler.ts
+++ packages/core/lib/v3/handlers/extractHandler.ts
@@ -148,11 +148,6 @@ export class ExtractHandler {
           ? { instruction: { value: instruction, type: "string" } }
           : undefined,
       });
-      // eslint-disable-next-line no-constant-condition
-      if (1 + 1 == 2) {
-        // do nothing
-        //TODO: remove this
-      }
 
       // Normalize schema: if instruction provided without schema, use defaultExtractSchema
       const baseSchema: StagehandZodSchema = (schema ??
