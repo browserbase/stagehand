b8fba7fb9cc606b359e2584404b491854cf15e07
monadoid <sam.finton@gmail.com>
2025-12-15 16:00:34 -0800

    test fix

--- packages/core/tests/public-api/export-surface.test.ts
+++ packages/core/tests/public-api/export-surface.test.ts
@@ -38,6 +38,7 @@ const publicApiShape = {
   loadApiKeyFromEnv: Stagehand.loadApiKeyFromEnv,
   localBrowserLaunchOptionsSchema: Stagehand.localBrowserLaunchOptionsSchema,
   modelToAgentProviderMap: Stagehand.modelToAgentProviderMap,
+  navigateResponseSchema: Stagehand.navigateResponseSchema,
   pageTextSchema: Stagehand.pageTextSchema,
   providerEnvVarMap: Stagehand.providerEnvVarMap,
   toGeminiSchema: Stagehand.toGeminiSchema,
