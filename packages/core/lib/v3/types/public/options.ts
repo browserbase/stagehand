dc11421ced9c28b993f5c86889c9776327252d19
Nick Sweeting <git@sweeting.me>
2025-12-18 16:29:20 -0800

    dont force zod v4 for core

--- packages/core/lib/v3/types/public/options.ts
+++ packages/core/lib/v3/types/public/options.ts
@@ -1,4 +1,4 @@
-import { z } from "zod/v4";
+import { z } from "zod";
 import { LLMClient } from "../../llm/LLMClient";
 import { ModelConfiguration } from "./model";
 import { LogLine } from "./logs";
