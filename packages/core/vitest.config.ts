741f778b04766247887c7e1662878368493ddd4a
Miguel <36487034+miguelg719@users.noreply.github.com>
2026-02-20 17:55:07 -0800

    chore: consolidate test directories in packages/core (#1723)

--- packages/core/vitest.config.ts
+++ packages/core/vitest.config.ts
@@ -12,6 +12,6 @@ export default defineConfig({
   },
   test: {
     environment: "node",
-    include: ["**/dist/esm/tests/**/*.test.js"],
+    include: ["**/dist/esm/tests/unit/**/*.test.js"],
   },
 });
