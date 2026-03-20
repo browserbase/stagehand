b9cbb691ed5de2df64e9b87efbbdaf8f797f76d5
Sean McGuire <seanmcguire1@outlook.com>
2026-03-19 17:35:48 -0700

    rm redundant routes

--- packages/server-v4/src/routes/v4/page/routes.ts
+++ packages/server-v4/src/routes/v4/page/routes.ts
@@ -1,7 +1,6 @@
 import type { FastifyPluginCallback, RouteOptions } from "fastify";
 
 import addInitScriptRoute from "./addInitScript.js";
-import asProtocolFrameTreeRoute from "./asProtocolFrameTree.js";
 import pageActionDetailsRoute from "./action/_actionId.js";
 import pageActionListRoute from "./action/index.js";
 import clickRoute from "./click.js";
@@ -11,14 +10,12 @@ import enableCursorOverlayRoute from "./enableCursorOverlay.js";
 import evaluateRoute from "./evaluate.js";
 import framesRoute from "./frames.js";
 import getFullFrameTreeRoute from "./getFullFrameTree.js";
-import getOrdinalRoute from "./getOrdinal.js";
 import goBackRoute from "./goBack.js";
 import goForwardRoute from "./goForward.js";
 import gotoRoute from "./goto.js";
 import hoverRoute from "./hover.js";
 import keyPressRoute from "./keyPress.js";
 import listAllFrameIdsRoute from "./listAllFrameIds.js";
-import mainFrameRoute from "./mainFrame.js";
 import mainFrameIdRoute from "./mainFrameId.js";
 import screenshotRoute from "./screenshot.js";
 import scrollRoute from "./scroll.js";
@@ -31,7 +28,6 @@ import titleRoute from "./title.js";
 import typeRoute from "./type.js";
 import urlRoute from "./url.js";
 import waitForLoadStateRoute from "./waitForLoadState.js";
-import waitForMainLoadStateRoute from "./waitForMainLoadState.js";
 import waitForSelectorRoute from "./waitForSelector.js";
 import waitForTimeoutRoute from "./waitForTimeout.js";
 import reloadRoute from "./reload.js";
@@ -54,16 +50,12 @@ const rawPageRoutes: RouteOptions[] = [
   addInitScriptRoute,
   targetIdRoute,
   mainFrameIdRoute,
-  mainFrameRoute,
   getFullFrameTreeRoute,
-  asProtocolFrameTreeRoute,
   listAllFrameIdsRoute,
-  getOrdinalRoute,
   titleRoute,
   urlRoute,
   framesRoute,
   setExtraHTTPHeadersRoute,
-  waitForMainLoadStateRoute,
   screenshotRoute,
   snapshotRoute,
   setViewportSizeRoute,
