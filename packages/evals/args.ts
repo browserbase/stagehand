aef66594a38a30c49eb6ffa34175f7898876b8e1
miguel <miguelg71921@gmail.com>
2026-04-06 10:06:17 -0700

    unclean evals overhaul

--- packages/evals/args.ts
+++ packages/evals/args.ts
@@ -88,7 +88,6 @@ const DEFAULT_EVAL_CATEGORIES = process.env.EVAL_CATEGORIES
       "extract",
       "experimental",
       "targeted_extract",
-      "regression_llm_providers",
       "regression",
       "agent",
       "external_agent_benchmarks",
