a8535c795cdd272a42e77c08622ed29bc9b51760
Nick Sweeting <github@sweeting.me>
2026-05-21 17:45:26 -0700

    Delete packages/server-v4 entirely for now (#2151)

--- packages/server-v4/src/utils.ts
+++
@@ -1,7 +0,0 @@
-export function omitUndefined<T extends Record<string, unknown>>(
-  value: T,
-): Partial<T> {
-  return Object.fromEntries(
-    Object.entries(value).filter(([, fieldValue]) => fieldValue !== undefined),
-  ) as Partial<T>;
-}
