3b93be15b4df3260d198700c32fa98af0e37f769
Sam F <43347795+monadoid@users.noreply.github.com>
2026-07-13 00:01:31 +0200

    remove unused filesystem cache (#14)
    Co-authored-by: samfinton <samfinton@samfintons-MacBook-Pro.local>

--- packages/extension/types/private/index.ts
+++ packages/extension/types/private/index.ts
@@ -2,6 +2,5 @@ export * from "./api.js";
 export * from "./handlers.js";
 export * from "./internal.js";
 export * from "./evaluator.js";
-export * from "./cache.js";
 export * from "./logger.js";
 export * from "./snapshot.js";
